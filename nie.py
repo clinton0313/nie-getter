2b1b27fa3dd2005caacb6a57e07d694844c574b2
clinton0313 <clinton0313@gmail.com>
2021-09-13 10:47:42 +0200

    fixed import issues

--- nie.py
+++ nie.py
@@ -1,9 +1,10 @@
-from selenium import webdriver as wd
-from selenium import Select
+from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
-driver = wd.Firefox
-driver.get("https://sede.administracionespublicas.gob.es/icpplustieb/citar?p=8&locale=es")
+driver = WebDriver
+
 
 nie = "Y8809001S"
 name = "Clinton Leung"
@@ -12,8 +13,9 @@ expiry = "16/12/2023"
 tel = "658427293"
 email = "clinton0313@gmail.com"
 
-#function for the page to select appointment type and continue
+#function for the page to select appointment type and continue (skips choosing city)
 def sel_apt_type():
+    driver.get("https://sede.administracionespublicas.gob.es/icpplustieb/citar?p=8&locale=es")
     select = Select(driver.find_element_by_id("tramiteGrupo[0]"))
     select.select_by_visible_text("POLICIA-TOMA DE HUELLAS (EXPEDICIÓN DE TARJETA) Y RENOVACIÓN DE TARJETA DE LARGA DURACIÓN")
     accept = driver.find_element_by_id("btnAceptar")
@@ -25,14 +27,18 @@ def info_page():
     entrar.click()
 
 def fill_info(nie, name, country, expiry):
-    nie_field = driver.find_element_by_id("txtIdCitadto")
+    nie_field = driver.find_element_by_id("txtIdCitado")
     name_field = driver.find_element_by_id("txtDesCitado")
-    country_select = Select(driver.find_element_by_id("txtPaisNac")
+    country_select = Select(driver.find_element_by_id("txtPaisNac"))
     expiry_field = driver.find_element_id("txtFecha")
 
 #fill fields
-
+    nie_field.send_keys(nie)
+    name_field.send_keys(name)
+    expiry_field.send_keys(expiry)
     country_select.select_by_visible_text(country)
+
+#hit accept
     accept = Select(driver.find_element_by_id("btnEnviar"))
     accept.click()
 
@@ -40,13 +46,24 @@ def cita_info(tel, email):
     tel_field = driver.find_element_by_id("txtTelefonoCitado")
     email_field = driver.find_element_by_id("emailUNO")
     emailconfirm_field = driver.find_element_by_id("emailDOS")
+    
+    tel_field.send_keys(tel)
+    email_field.send_keys(email)
+    emailconfirm_field.send_keys(email)
+    
     siguiente = Select(driver.find_element_by_id("btnSiguiente"))
     siguiente.click()
     #check this is how to click
-    #fill fields
 
 def select_office():
-    office = Select(driver.find_element_by_id("idSede")
+    office = Select(driver.find_element_by_id("idSede"))
     siguiente = Select(driver.find_element_by_id("btnSiguiente"))
     siguiente.click()
 
+sel_apt_type
+info_page
+fill_info(nie, name, country, expiry)
+#if statement
+#cita_info(tel, email)
+#select_office
+#alert or restart
